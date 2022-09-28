7b3f6b74901ce73b0dc8f3d7d35f5e7d51632613
Adeola Esther <estheradeola803@gmail.com>
2022-09-28 19:03:46 +0100

    Updated code

--- src/practiseCode/chapterFouteenExamples/CharMethod.java
+++ src/practiseCode/chapterFouteenExamples/CharMethod.java
@@ -8,10 +8,10 @@ public class CharMethod {
         System.out.printf("c1= %s%nc2 =%s%n%n", c1.charValue(),c2.toString());
 
         if(c1.equals(c2)){
-            System.out.println("c1 and c2 are equal%n");
+            System.out.println("c1 and c2 are equal");
         }
         else {
-            System.out.println("c1 and c2 are not equal%n");
+            System.out.println("c1 and c2 are not equal");
         }
     }
 }
