0ce62d32ca89660b2e585e19fa5105fd8c6fa9d2
Adeola Esther <estheradeola803@gmail.com>
2022-09-28 19:04:59 +0100

    Updated code

--- src/practiseCode/chapterFouteenExamples/CharMethod.java
+++ src/practiseCode/chapterFouteenExamples/CharMethod.java
@@ -4,7 +4,7 @@ public class CharMethod {
 
     public static void main(String[] args) {
         Character c1='R';
-        Character c2= 'r';
+        Character c2= 'R';
         System.out.printf("c1= %s%nc2 =%s%n%n", c1.charValue(),c2.toString());
 
         if(c1.equals(c2)){
