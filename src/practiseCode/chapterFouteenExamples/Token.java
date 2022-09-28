c4b16ca3b000bdfade0883fec6a260c8dda0c3a3
Adeola Esther <estheradeola803@gmail.com>
2022-09-28 20:32:15 +0100

    Updated code

---
+++ src/practiseCode/chapterFouteenExamples/Token.java
@@ -0,0 +1,19 @@
+package practiseCode.chapterFouteenExamples;
+
+import java.util.Scanner;
+
+public class Token {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter a sentence and press Enter");
+        String sentence = scan.nextLine();
+
+        String[] tokens= sentence.split(" ");
+        System.out.printf("Number of elements: %d%nThe tokens are: %n", tokens.length);
+
+        for (String token: tokens){
+            System.out.println(token);
+        }
+    }
+
+}
