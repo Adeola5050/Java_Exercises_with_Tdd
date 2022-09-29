4a46912edc4bce2cd17865146481bb5057ea473a
Adeola Esther <estheradeola803@gmail.com>
2022-09-29 16:24:08 +0100

    updated code

---
+++ src/chapterFourteen/WordsReversed.java
@@ -0,0 +1,18 @@
+package chapterFourteen;
+
+public class WordsReversed {
+    public static void main(String[] args) {
+        String words= "The major of the barracks is handsome";
+        System.out.printf("The original words is %s%n",words);
+        String [] tokens=words.split(" ");
+
+        for (String token: tokens){
+        System.out.println(token);
+    }
+
+        for (int count = words.length()-1; count >=0 ; count--) {
+            System.out.printf("%c: ", words.charAt(count));
+
+        }
+    }
+}
